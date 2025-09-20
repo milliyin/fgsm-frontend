d4632fde7210d44dd2e27d6455d5c98604161c30
Muhammad Illiyin <55222179+milliyin@users.noreply.github.com>
2025-09-21 01:22:03 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 "use client"
 import React, { useState, useRef } from 'react';
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+const client = generateClient<Schema>();
 
 export default function Home() {
   const [file, setFile] = useState<File | null>(null);
